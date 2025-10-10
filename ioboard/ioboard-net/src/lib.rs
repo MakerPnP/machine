6d3e6f0a8797fb4429396762b8007a65a58a54b8
Dominic Clifton <me@dominicclifton.name>
2025-10-10 11:50:10 +0200

    Additional tracing for network writes.

--- ioboard/ioboard-net/src/lib.rs
+++ ioboard/ioboard-net/src/lib.rs
@@ -1,12 +1,11 @@
 #![no_std]
 
 use core::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
-
 use embedded_io_async::Write;
 use embedded_nal_async::TcpConnect;
 use ioboard_time::TimeService;
 use ioboard_trace::tracepin;
-use log::info;
+use log::{info, error};
 
 pub struct IoConnection<TIME: TimeService, CLIENT: TcpConnect> {
     time: TIME,
@@ -26,10 +25,10 @@ impl<TIME: TimeService, CLIENT: TcpConnect> IoConnection<TIME, CLIENT> {
             // You need to start a server on the host machine, for example: `nc -l 8000`
             let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(192, 168, 18, 41), 8000));
 
-            info!("connecting...");
+            info!("Connecting...");
             let r = self.client.connect(addr).await;
             if let Err(e) = r {
-                info!("connect error: {:?}", e);
+                error!("Connect error: {:?}", e);
                 self.time
                     .delay_until_us(self.time.now_micros() + 1_000_000)
                     .await;
@@ -42,6 +41,7 @@ impl<TIME: TimeService, CLIENT: TcpConnect> IoConnection<TIME, CLIENT> {
             let cycle_period_us = 1_000_000 / 10;
             let mut deadline = self.time.now_micros();
             loop {
+                tracepin::on(2);
                 let r = connection.write_all(
                     b"\
                     0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\
@@ -50,8 +50,9 @@ impl<TIME: TimeService, CLIENT: TcpConnect> IoConnection<TIME, CLIENT> {
                     0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\
                     \n"
                 ).await;
+                tracepin::off(2);
                 if let Err(e) = r {
-                    info!("write error: {:?}", e);
+                    error!("write error: {:?}", e);
                     break;
                 }
                 deadline += cycle_period_us;
