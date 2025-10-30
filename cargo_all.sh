a2f014ee22a7eedca09d3529001a725842849443
Dominic Clifton <me@dominicclifton.name>
2025-10-30 03:33:31 +0100

    Add cargo_all.sh script.

---
+++ cargo_all.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+args="$*"
+
+# List of directories to process
+dirs=("common" "firmware" "ioboard" "operator_ui" "server")
+
+# Iterate through each directory in the list
+for dir in "${dirs[@]}"; do
+    if [ -d "$dir" ]; then
+        pushd "$dir" > /dev/null
+
+        if [ -f "Cargo.toml" ]; then
+            echo "running command in '$dir':"
+            eval "cargo $args"
+        else
+
+            # Run the cargo command in each subdirectory
+            for f in *; do
+                if [ -d "$f" ]; then
+                    if [ -d "$f" ] && [ -f "$f/Cargo.toml" ]; then
+                        pushd "$f" > /dev/null
+                        echo "running command in '$f':"
+                        eval "cargo $args"
+                        popd > /dev/null
+                    fi
+                fi
+            done
+        fi
+
+        popd > /dev/null
+    fi
+done
