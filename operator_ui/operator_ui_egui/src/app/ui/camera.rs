2c77d46ad4ee756f19a14b9adab8f2a85249a5aa
Dominic Clifton <me@dominicclifton.name>
2025-11-10 14:41:40 +0100

    Add egui_vertical_stack and egui_tool_windows deps. Use a tool window for camera stats.
    * also ensured that the position of the tool window is retained when changing workspaces by assigning a persistent id to the tool window.

--- operator_ui/operator_ui_egui/src/app/ui/camera.rs
+++ operator_ui/operator_ui_egui/src/app/ui/camera.rs
@@ -1,6 +1,9 @@
 use eframe::epaint::ColorImage;
 use eframe::epaint::textures::TextureOptions;
-use egui::{Image, Ui, Widget};
+use egui::{Frame, Image, Ui, Widget};
+use egui_i18n::tr;
+use egui_mobius::Value;
+use egui_tool_windows::ToolWindows;
 use tokio::sync::watch::Receiver;
 use tracing::trace;
 
@@ -12,7 +15,7 @@ pub(crate) struct CameraUi {
     texture: Option<egui::TextureHandle>,
 
     camera_frame_number: u64,
-    camera_fps_stats: FpsStats,
+    camera_fps_stats: Value<FpsStats>,
     camera_fps_snapshot: Option<FpsSnapshot>,
 }
 
@@ -22,7 +25,7 @@ impl CameraUi {
             rx,
             texture: None,
 
-            camera_fps_stats: FpsStats::new(300),
+            camera_fps_stats: Value::new(FpsStats::new(300)),
             camera_fps_snapshot: None,
             camera_frame_number: 0,
         }
@@ -36,11 +39,15 @@ impl CameraUi {
         if let Ok(true) = self.rx.has_changed() {
             let color_image = self.rx.borrow_and_update().clone();
             self.camera_frame_number += 1;
-            self.camera_fps_snapshot = self.camera_fps_stats.update(now);
-            trace!(
-                "received frame, now: {:?}, frame_number: {}, snapshot: {:?}",
-                now, self.camera_frame_number, self.camera_fps_snapshot
-            );
+            if let Ok(snapshot) = self.camera_fps_stats.lock().map(|mut fps_stats| {
+                fps_stats.update(now)
+            }) {
+                self.camera_fps_snapshot = snapshot;
+                trace!(
+                    "received frame, now: {:?}, frame_number: {}, snapshot: {:?}",
+                    now, self.camera_frame_number, self.camera_fps_snapshot
+                );
+            }
 
             if let Some(tex) = &mut self.texture {
                 tex.set(color_image, TextureOptions::default());
@@ -60,33 +67,39 @@ impl CameraUi {
                     .maintain_aspect_ratio(true)
                     .ui(ui);
             } else {
-                ui.label("Waiting for first frame...");
+                ui.label(tr!("camera-message-waiting"));
             }
         });
 
-        // TODO use a tool window here, constrained to the camera tile/window.
+        let fps_stats_id = ui.make_persistent_id(ui.id().with("camera-toolwindow-fps-stats"));
+        ToolWindows::new()
+            .windows(ui, |builder|{
+                builder
+                    .add_window(fps_stats_id)
+                    .default_pos([10.0, 10.0])
+                    .default_size([400.0, 150.0])
+                    .show(tr!("camera-toolwindow-fps-stats-title"), {
+                        let camera_fps_stats = self.camera_fps_stats.clone();
+                        let camera_fps_snapshot = self.camera_fps_snapshot.clone();
+                        let camera_frame_number = self.camera_frame_number;
 
-        egui::Area::new(ui.id().with("camera_stats"))
-            .interactable(true)
-            .movable(true)
-            .show(ui.ctx(), |ui| {
-                egui::Frame::window(ui.style()).show(ui, |ui| {
-                    egui::Resize::default().show(ui, |ui| {
-                        ui.add(egui::Label::new("Stats").selectable(false));
-                        ui.separator();
-                        egui::ScrollArea::both().show(ui, |ui| {
-                            ui.label(format!("Frame: {}", self.camera_frame_number));
-                            if let Some(snapshot) = &self.camera_fps_snapshot {
-                                ui.label(format!(
-                                    "FPS: {:.1} (min {:.1}, max {:.1}, avg {:.1})",
-                                    snapshot.latest, snapshot.min, snapshot.max, snapshot.avg
-                                ));
+                        move |ui| {
+                            egui::ScrollArea::both().show(ui, |ui| {
+                                Frame::group(ui.style()).show(ui, |ui|{
+                                    ui.label(format!("Frame: {}", camera_frame_number));
+                                    if let Some(snapshot) = &camera_fps_snapshot {
+                                        ui.label(format!(
+                                            "FPS: {:.1} (min {:.1}, max {:.1}, avg {:.1})",
+                                            snapshot.latest, snapshot.min, snapshot.max, snapshot.avg
+                                        ));
 
-                                show_frame_durations(ui, &self.camera_fps_stats);
-                            }
-                        })
+                                        let camera_fps_stats = camera_fps_stats.lock().unwrap();
+                                        show_frame_durations(ui, &camera_fps_stats);
+                                    }
+                                });
+                            });
+                        }
                     });
-                });
             });
     }
 }
