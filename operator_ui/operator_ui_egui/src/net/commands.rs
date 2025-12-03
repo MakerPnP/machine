265ba8062d4823c513fe0f64e86610e4e412856f
Dominic Clifton <me@dominicclifton.name>
2025-12-03 17:22:57 +0100

    OperatorUI - Refactor the heartbeat sender to use the shutdown handler only once.

--- operator_ui/operator_ui_egui/src/net/commands.rs
+++ operator_ui/operator_ui_egui/src/net/commands.rs
@@ -20,6 +20,17 @@ endpoint!(
 pub async fn heartbeat_sender(stack: EdgeStack, address: Address, app_event_rx: Receiver<AppEvent>) {
     let mut app_shutdown_handler = Box::pin(app_shutdown_handler(app_event_rx));
 
+    select! {
+        _ = &mut app_shutdown_handler => {
+            // Shutdown received
+        }
+        _ = heartbeat_loop(stack, address) => {
+            // Heartbeat loop completed (shouldn't happen unless there's an error)
+        }
+    }
+}
+
+async fn heartbeat_loop(stack: EdgeStack, address: Address) {
     let command_client = stack
         .endpoints()
         .client::<OperatorCommandEndpoint>(address, None);
@@ -27,42 +38,33 @@ pub async fn heartbeat_sender(stack: EdgeStack, address: Address, app_event_rx: 
 
     let mut index = 0;
     tokio::time::sleep(Duration::from_secs(1)).await;
+
     let heartbeat_timeout_duration = Duration::from_secs(10);
     let heartbeat_send_interval = heartbeat_timeout_duration / 2;
     let mut ticker = time::interval(heartbeat_send_interval);
 
     loop {
-        // At either stage (waiting response or waiting for tick) we could receive a shutdown event
+        // Wait for tick
+        ticker.tick().await;
 
+        // Send heartbeat
         let request = OperatorCommandRequest::Heartbeat(index);
-        select! {
-            response = command_client.request(&request) => {
+        match command_client.request(&request).await {
+            Ok(response) => {
                 match response {
-                    Ok(response) => {
-                        match response {
-                            OperatorCommandResponse::Acknowledged => {
-                                index = index.wrapping_add(1);
-                            }
-                            _ => {}
-                        }
+                    OperatorCommandResponse::Acknowledged => {
+                        // Success - proceed to next iteration
                     }
-                    Err(e) => {
-                        error!("error sending heartbeat. index: {}, error: {:?}", index, e);
+                    _ => {
+                        error!("Unexpected response for heartbeat. index: {}", index);
                     }
                 }
             }
-            _ = &mut app_shutdown_handler => {
-                break
+            Err(e) => {
+                error!("Error sending heartbeat. index: {}, error: {:?}", index, e);
             }
         }
 
-        select! {
-            _ = ticker.tick() => {
-                index = index.wrapping_add(1);
-            }
-            _ = &mut app_shutdown_handler => {
-                break
-            }
-        }
+        index = index.wrapping_add(1);
     }
 }
