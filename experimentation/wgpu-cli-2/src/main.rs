cdcd61309ad0dde03604f77205e29f61a80756a8
Dominic Clifton <me@dominicclifton.name>
2025-12-15 17:55:11 +0100

    WGPU-cli-2 - fix incorrect culling mode.

--- experimentation/wgpu-cli-2/src/main.rs
+++ experimentation/wgpu-cli-2/src/main.rs
@@ -138,7 +138,7 @@ impl RenderState {
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Cw, // Clockwise for CAD models
+                front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
                 polygon_mode: wgpu::PolygonMode::Fill,
                 unclipped_depth: false,
