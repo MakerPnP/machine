c285bffa26b2cb1fc6c3abd531b4f7c5ae5d7325
Dominic Clifton <me@dominicclifton.name>
2026-06-18 12:08:37 +0200

    Firmware - Remove dummy read, now that a work-around is in-place.

--- firmware/firmware-makerpnpcontrolcore/stm32h7/src/fpga/mod.rs
+++ firmware/firmware-makerpnpcontrolcore/stm32h7/src/fpga/mod.rs
@@ -67,7 +67,6 @@ impl<I: Instance> FpgaCore<I> {
         defmt::assert!(self.memory_mapped_mode_enabled);
 
         let value = fpga_pac::IO.io_in_1().read();
-        let dummy = fpga_pac::SYSTEM1.ident().read();
 
         let buttons = (value.user0() as u8) | ((value.user1() as u8) << 1);
         defmt::debug!("FPGA value: 0x{:08x}, buttons: 0b{:02b}", value.0, buttons);
