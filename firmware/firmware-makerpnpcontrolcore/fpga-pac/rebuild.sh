1051edf58079a021fd1d9c80f10a7978283f3a98
Dominic Clifton <me@dominicclifton.name>
2026-06-18 12:30:50 +0200

    Add patches. Update rebuild script to apply them.

--- firmware/firmware-makerpnpcontrolcore/fpga-pac/rebuild.sh
+++ firmware/firmware-makerpnpcontrolcore/fpga-pac/rebuild.sh
@@ -2,3 +2,7 @@
 
 chiptool generate --svd ../fpga/assets/svd/fpga.svd --output src
 rustfmt src/lib.rs
+git apply < patches/0001-delete-interrupt-block.patch
+git apply < patches/0002-delete-rt-block.patch
+git apply < patches/0003-octospi-fifo-bypass.patch
+rm src/device.x
