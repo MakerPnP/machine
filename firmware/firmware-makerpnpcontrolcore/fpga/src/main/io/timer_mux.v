4e7d2f43078bbbd9784867f039b6c8225b9fb603
Dominic Clifton <me@dominicclifton.name>
2026-06-25 19:03:18 +0200

    FPGA - cleanup old timer_mux,wake and clock_out which have yet to be updated.

--- firmware/firmware-makerpnpcontrolcore/fpga/src/main/io/timer_mux.v
+++ firmware/firmware-makerpnpcontrolcore/fpga/src/main/io/timer_mux.v
@@ -3,23 +3,24 @@ module timer_mux
     (
         input  wire        sys_clk,
         input  wire        reset,
-        output reg mux_sel1,
-        output reg mux_sel2,
-        output reg mux_sel3,
-        output reg mux_sel4
+        output wire mux_sel1,
+        output wire mux_sel2,
+        output wire mux_sel3,
+        output wire mux_sel4
     );
+
+reg[3:0] mux_sel;
+
+assign mux_sel1 = mux_sel[0];
+assign mux_sel2 = mux_sel[1];
+assign mux_sel3 = mux_sel[2];
+assign mux_sel4 = mux_sel[3];
 
 always @(posedge sys_clk) begin
     if (reset) begin
-        mux_sel1 = 1;
-        mux_sel2 = 1;
-        mux_sel3 = 1;
-        mux_sel4 = 1;
+        mux_sel <= 4'b1111;
     end else begin
-        mux_sel1 = 0;
-        mux_sel2 = 0;
-        mux_sel3 = 0;
-        mux_sel4 = 0;
+        mux_sel <= 4'b0000;
     end
 end
 
--- firmware/firmware-makerpnpcontrolcore/fpga/src/main/io/wake.v
+++ firmware/firmware-makerpnpcontrolcore/fpga/src/main/io/wake.v
@@ -4,29 +4,24 @@ module wake
         input  wire        sys_clk,
         input  wire        reset,
         input              nwake_in,
-        output reg nwake_1,
-        output reg nwake_2,
-        output reg nwake_3,
-        output reg nwake_4
+        output wire nwake_1,
+        output wire nwake_2,
+        output wire nwake_3,
+        output wire nwake_4
     );
 
     reg nwake_in_r;
 
-    always @(*) begin
-        nwake_in_r = nwake_in;
-    end
+    assign nwake_1 = nwake_in_r;
+    assign nwake_2 = nwake_in_r;
+    assign nwake_3 = nwake_in_r;
+    assign nwake_4 = nwake_in_r;
 
     always @(posedge sys_clk) begin
         if (reset) begin
-            nwake_1 = 1;
-            nwake_2 = 1;
-            nwake_3 = 1;
-            nwake_4 = 1;
+            nwake_in_r = 1'b1;
         end else begin
-            nwake_1 <= nwake_in_r;
-            nwake_2 <= nwake_in_r;
-            nwake_3 <= nwake_in_r;
-            nwake_4 <= nwake_in_r;
+            nwake_in_r = nwake_in;
         end
     end
 
